d96c9498b9ab4aea63eee5249c6b43faa0d7ca2e
medo67 <45091642+medo67@users.noreply.github.com>
2019-01-20 20:57:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("Relax Server",{type: 'WATCHING'})
+   client.user.setActivity("Relax Server",{type: 'STREAMING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
